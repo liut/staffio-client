7cdd36110cad841a050cbe9dd182845efb61dde5
liut <liut@phoenixtv.com>
2019-10-25 00:06:27 +0800

    Migrate from `staffio/client`

---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/liut/staffio-client
+
+go 1.12
+
+require (
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/gin-gonic/gin v1.4.0
+	github.com/go-osin/session v2.1.0+incompatible
+	github.com/icza/mighty v0.0.0-20180919140131-cfd07d671de6 // indirect
+	github.com/json-iterator/go v1.1.7 // indirect
+	github.com/liut/simpauth v0.0.1
+	github.com/mattn/go-isatty v0.0.9 // indirect
+	github.com/philhofer/fwd v1.0.0 // indirect
+	github.com/tinylib/msgp v1.1.0 // indirect
+	github.com/ugorji/go/codec v1.1.7 // indirect
+	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45
+)
---
+++ token.go
@@ -0,0 +1,75 @@
+package client
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strings"
+	"time"
+
+	"golang.org/x/oauth2"
+
+	auth "github.com/liut/simpauth"
+)
+
+// InfoToken ...
+type InfoToken struct {
+	AccessToken  string     `json:"access_token"`
+	TokenType    string     `json:"token_type,omitempty"`
+	RefreshToken string     `json:"refresh_token,omitempty"`
+	ExpiresIn    int64      `json:"expires_in,omitempty"`
+	Expiry       time.Time  `json:"expiry,omitempty"`
+	Me           Staff      `json:"me,omitempty"`
+	Roles        auth.Names `json:"group,omitempty"`
+}
+
+// GetExpiry ...
+func (tok *InfoToken) GetExpiry() time.Time {
+	return time.Now().Add(time.Duration(tok.ExpiresIn) * time.Second)
+}
+
+type infoError struct {
+	Code    string `json:"error,omitempty"`
+	Message string `json:"error_description,omitempty"`
+}
+
+func (e *infoError) Error() string {
+	return fmt.Sprintf("%s: %s", e.Code, e.Message)
+}
+
+// RequestInfoToken ...
+func RequestInfoToken(tok *oauth2.Token, roles ...string) (*InfoToken, error) {
+	client := conf.Client(oauth2.NoContext, tok)
+	uri := infoURI
+	if len(roles) > 0 {
+		uri = infoURI + "+" + strings.Join(roles, "+")
+	}
+	info, err := client.Get(uri)
+	if err != nil {
+		return nil, err
+	}
+	defer info.Body.Close()
+	data, err := ioutil.ReadAll(info.Body)
+	if err != nil {
+		log.Printf("read infoURI err %s", err)
+		return nil, err
+	}
+
+	infoErr := &infoError{}
+	if e := json.Unmarshal(data, infoErr); e != nil {
+		return nil, e
+	}
+
+	if infoErr.Code != "" {
+		return nil, infoErr
+	}
+
+	var token = &InfoToken{}
+	err = json.Unmarshal(data, token)
+	if err != nil {
+		log.Printf("unmarshal to infoToken err %s", err)
+		return nil, err
+	}
+	return token, nil
+}
